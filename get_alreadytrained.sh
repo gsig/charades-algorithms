e8d81fe4eb5ea01631baa86c8c469d48313500ab
Gunnar Sigurdsson <gsigurds@cs.cmu.edu>
2017-05-02 18:26:19 -0400

    Adding pretrained models on Charades

---
+++ get_alreadytrained.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to download pretrained models on Charades
+# Approximately equivalent to models obtained by running exp/rgbnet.lua and exp/flownet.lua
+#
+# The flow model was obtained after 62 epochs (epochSize=0.1)
+# The flow model has a classification accuracy of 15.4% mAP (via charades_v1_classify.m)
+# The rgb model was obtained after 6 epochs (epochSize=0.1)
+# The rgb model has a classification accuracy of 15.6% mAP (via charades_v1_classify.m)
+#
+# Combining the predictions (submission files) of those models using combine_rgb_flow.py
+# yields a final classification accuracy of 18.9% mAP (via charades_v1_classify.m)
+
+wget https://dl.dropboxusercontent.com/u/10728218/models/twostream_flow.t7
+wget https://dl.dropboxusercontent.com/u/10728218/models/twostream_rgb.t7
--- get_models.sh
+++ get_models.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script to download pretrained models that are the starting point for training
+# Script to download models pretrained on ImageNet and UCF101
+# Those are used as the starting point for training on Charades
 
 wget https://dl.dropboxusercontent.com/u/10728218/models/VGG_ILSVRC_16_layers_deploy.prototxt
 wget https://dl.dropboxusercontent.com/u/10728218/models/VGG_UCF101_16_layers_deploy.prototxt
